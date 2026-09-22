5626d296d1ca2c3cc9c647cd4d7efbc10db8332f
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-23 00:29:55 +0300

    Set up base files and arithmetic functions for calculator project
    - Create index.html and link script.js for browser console testing
    - Implement add(), subtract(), multiply(), and divide() basic functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = (...num) => {
+  const result = num.reduce((acc, currentValue) => acc + currentValue);
+  return result;
+};
+
+const subtract = (...num) => {
+  const result = num.reduce((acc, currentValue) => acc - currentValue);
+  return result;
+};
+
+const multiply = (...num) => {
+  const result = num.reduce((acc, currentValue) => acc * currentValue);
+  return result;
+};
+
+const divide = (...num) => {
+  const result = num.reduce((acc, currentValue) => acc * currentValue);
+  return result;
+};
