99ccafe1bb9423638e9a4d4e3ef41dcf46e4712f
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-23 10:30:56 +0300

    Implement operate function in script.js
    - Accept two numbers and an operator as arguments
    - Call corresponding arithmetic function (add, subtract, multiply, divide)
    - Return calculation result

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ const divide = (...num) => {
   const result = num.reduce((acc, currentValue) => acc * currentValue);
   return result;
 };
+
+let num1;
+let num2;
+let operator;
+
+const operate = (num1, operator, num2) => {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "x":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+  }
+
+  return result;
+};
+
+console.log(operate(2, "+", 4));
